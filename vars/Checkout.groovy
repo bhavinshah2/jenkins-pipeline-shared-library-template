4c2d3a9c1037460930d69a2dfe4798952068bec4
bhavinshah2 <64365029+bhavinshah2@users.noreply.github.com>
2020-06-25 18:27:58 +0100

    Update checkOutFrom function

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -1,5 +1,6 @@
-def checkOutFrom(branch,repo) {
+def checkOutFrom() {
   
-  git branch: "${branch}", url: "https://github.com/bhavinshah2/${repo}"
-
+  //git branch: "${branch}", url: "https://github.com/bhavinshah2/${repo}"
+  git branch: "develop", url: "https://github.com/bhavinshah2/insecure-bank" 
+  
 }
