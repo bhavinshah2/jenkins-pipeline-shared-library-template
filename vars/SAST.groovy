1755ddf78ec839b917fad6ff0340cb5efc09e67c
bhavinshah2 <64365029+bhavinshah2@users.noreply.github.com>
2020-06-26 13:41:05 +0100

    Update and rename SAST.groovy to sast.groovy

--- vars/SAST.groovy
+++
@@ -1,5 +0,0 @@
-def SonarQube {
-  
-  mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar -Dsonar.host.url=http://1863cb34bb68.ngrok.io'
-  
-}
---
+++ vars/sast.groovy
@@ -0,0 +1,5 @@
+def SonarQube {
+  
+  mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar -Dsonar.host.url='http://ef52dd317743.ngrok.io'
+  
+}
